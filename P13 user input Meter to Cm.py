f5a2122ced2ee4a36ecbd1110bf38231a52f22d2
Raj Kachhadiya <rajkachhadiya6236@gmail.com>
2024-08-14 16:59:43 +0530

    Basic Programs

---
+++ P13 user input Meter to Cm.py
@@ -0,0 +1,4 @@
+M=int(input("Enter Meter ="))
+Cm=M*100
+print(M,"Meter =",Cm)
+print(M,"cm =",M/100)
---
+++ P9 user input Previous Next Nu.py
@@ -0,0 +1,5 @@
+A=int(input("Enter Number ="))
+Prev=A-1
+Next=A+1
+print("previous Number =",Prev)
+print("Next Number =",Next)
