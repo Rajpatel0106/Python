13b642cf94e4a0ad5f54b1d66e85ac96acce8bff
Raj Kachhadiya <rajkachhadiya05@gmail.com>
2024-08-22 21:37:07 +0530

    Basic

--- If_elif/P32 Xerox or type and Bill.py
+++ If_elif/P32 Xerox or type and Bill.py
@@ -1,5 +1,6 @@
 print("No 1 For Xerox")
 print("No 2 For Type")
+print("No 3 For Both")
 op = int(input("Enter Number ="))
 
 if op == 1:
@@ -11,12 +12,32 @@ if op == 1:
         bill1 = qt * 7
         print("Price = 7, Total Bill =", bill1)
 elif op == 2:
-    qt1 = int(input("How many Words ="))
-    if qt1 > 50:
-        bill = qt1 * 15
+    qt = int(input("How many Words ="))
+    if qt > 50:
+        bill = qt * 15
         print("Price = 15, Total Bill =", bill)
     else:
-        bill1 = qt1 * 20
+        bill1 = qt * 20
         print("Price = 20, Total Bill =", bill1)
+elif op == 3:
+    qt1 = int(input("How many Pages ="))
+    qt2 = int(input("How many Word ="))
+
+    if qt1 > 50:
+        bill = qt1 * 5
+        print("Price = 5, Total Bill =", bill)
+    else:
+        bill = qt1 * 7
+        print("Price = 7, Total Bill =", bill)
+
+    if qt2 > 50:
+        bill1 = qt2 * 15
+        print("Price = 15, Total Bill =", bill1)
+    else:
+        bill1 = qt2 * 20
+        print("Price = 20, Total Bill =", bill1)
+
+        Total = bill + bill1
+        print("Total Bill =", Total)
 else:
     print("Wrong option")
