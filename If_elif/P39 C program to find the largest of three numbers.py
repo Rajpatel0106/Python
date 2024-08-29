79b5ef3946f8851c9d0d2880826c2ff313f7d384
Raj Kachhadiya <rajkachhadiya05@gmail.com>
2024-08-29 18:29:51 +0530

    Basic

--- If_elif/P39 C program to find the largest of three numbers.py
+++ If_elif/P39 C program to find the largest of three numbers.py
@@ -4,7 +4,7 @@ No3 = int(input("Enter Number3 ="))
 
 if NO1 > No2 and NO1 > No3:
     print("No1 Is Largest")
-elif No2 > No3 and No2 > NO1:
+elif No2 > NO1 and No2 > No3:
     print("No2 Is Largest")
 elif No3 > NO1 and No3 > No2:
     print("No3 Is Largest")
